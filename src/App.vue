e8e6746d9a2f2d18dc0d7f836c75755aed0cdb6d
Jesse G <jesse.griffis@nbtsolutions.com>
2018-05-15 14:29:01 -0400

    missing something super extra special critical

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <v-thing></v-thing>
+    <v-fuck></v-fuck>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
---
+++ src/components/FuckYou.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fuck">FUCK YOU</div>
+</template>
+<style>
+.fuck {
+  color: blue;
+}
+</style>
+<script>
+import Vue from 'vue';
+
+export default {
+  name: 'v-fuck',
+};
+//Vue.component('v-fuck', {});
+</script>
